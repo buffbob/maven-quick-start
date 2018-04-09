97a3fc02c7dc1555cf346735b171d02a5d78255e
Robert <lastgulch@gmail.com>
2018-04-08 21:51:14 -0600

    Use StringUtils and add dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,15 +2,22 @@ package clinic.programming.trainning;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
-
     public static List<String> names;
 
     {
         names = new ArrayList<>();
         names.add("Fred");
         names.add("Jim");
+    }
+
+    public int countWords(String line){
+        String[] words = StringUtils.split(line," ");
+        int len = (words == null) ? 0:words.length;
+        return len;
     }
 
     public void greetFriends(List<String> nameList){
@@ -29,6 +36,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
     	new Application().greetFriends(names);
+    	new Application().countWords("how many words is ther e?");
 
     }
 }
