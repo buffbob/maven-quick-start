6eb13a91ed2595d3b4e55c24c213289a003c0211
Robert <lastgulch@gmail.com>
2018-04-08 21:18:36 -0600

    Changing source to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,34 @@
 package clinic.programming.trainning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    
+
+    public static List<String> names;
+
+    {
+        names = new ArrayList<>();
+        names.add("Fred");
+        names.add("Jim");
+    }
+
+    public void greetFriends(List<String> nameList){
+
+
+        for (String s : nameList) {
+            System.out.println("hello " + s);
+        }
+    }
     public Application() {
+
         System.out.println ("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+    	new Application().greetFriends(names);
+
     }
 }
